aef4b55b9b3b6a651b0de11819771e9bd2214fab
caio.cp <caio.cesar@itarget.com.br>
2022-08-16 10:42:58 -0300

    :wrench: chore: adicionando dependências para babel.

---
+++ components/SendMoney/SendMoneySection.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react";
+import styled from "styled-components/native";
+
+import { colors } from "../colors";
+
+import RegularText from "../Text/RegularText";
+import SmallText from "../Text/SmallText";
+
+import { SendMoneySectionProps } from "./types";
+
+const SendMoneySection: FunctionComponent<SendMoneySectionProps> = (props) => {
+  return <></>;
+};
+
+export default SendMoneySection;
