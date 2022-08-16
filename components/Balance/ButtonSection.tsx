9d6c158c5b4c1e4ad3c14b998b15a0271ef23268
caio.cp <caio.cesar@itarget.com.br>
2022-08-16 15:48:50 -0300

    :sparkles: feat: Adicionando o visual interno do cartão.

--- components/Balance/ButtonSection.tsx
+++ components/Balance/ButtonSection.tsx
@@ -2,9 +2,13 @@ import React, { FunctionComponent } from "react";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 
+import { useNavigation } from "@react-navigation/native";
+
 import { colors } from "../colors";
 
 import RegularButton from "../Button/RegularButton";
+
+import { Props as BalanceProps } from "../../screens/Balance";
 
 const ButtonSectionBackground = styled.View`
   width: 100%;
@@ -14,9 +18,15 @@ const ButtonSectionBackground = styled.View`
 `;
 
 const ButtonSection: FunctionComponent = () => {
+  const navigation = useNavigation<BalanceProps["navigation"]>();
+
+  const handlePress = () => {
+    navigation.navigate("Home");
+  };
+
   return (
     <ButtonSectionBackground>
-      <RegularButton btnStyles={{ width: "50%" }} onPress={() => {}}>
+      <RegularButton btnStyles={{ width: "50%" }} onPress={() => handlePress()}>
         Cancel <Ionicons name="card" size={17} color={colors.white} />
       </RegularButton>
     </ButtonSectionBackground>
